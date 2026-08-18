94ec34f28b4bea560119bce5da59c3f0d29d47c0
Ayyybb21 <brandonblascak@yahoo.com>
2026-08-17 22:13:06 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="td-survivor-v9.10";
-const ASSETS=["./","./index.html","./styles.css","./config.js","./app-v9.js","./manifest.webmanifest","./icon.svg"];
+const CACHE="td-survivor-v9.10.1";
+const ASSETS=["./","./index.html","./styles.css","./config.js","./app-v910.js","./manifest.webmanifest","./icon.svg"];
 
 self.addEventListener("install",event=>{
   self.skipWaiting();
